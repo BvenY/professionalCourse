7660253eb47cf75ad86a332adc744f0da8d646c0
Yangbowen <bestbven@163.com>
2019-11-23 15:52:09 +0800

    最终版本 哪个儿再改

--- pid.js
+++ pid.js
@@ -1,17 +1,14 @@
-"use strict";
 
 (function() {
-
     ////////// DISPLAY RELATED //////////
     // 设置画布
     let c = document.getElementById("c");
     // 获取画布
     let ctx = c.getContext("2d");
-    let print = document.getElementById("print");
 
     // 设置画布的高度宽度
     function resizeCanvas() {
-        c.width = window.innerWidth;
+        c.width = window.innerWidth * 0.7;
         c.height = window.innerHeight;
     }
     resizeCanvas();
@@ -19,17 +16,18 @@
     window.addEventListener("resize", resizeCanvas);
 
     ////////// PID //////////
-    // PID控制的点默认的坐标
-    let x = 700;
+    // 系统控制核心点的坐标值
+    let x = 400;
     let y = 300;
-    // PID控制的点默认的坐标变化量
+    // 核心点内小圆展示变化趋势
     let vx = 0;
     let vy = 0;
-
-    // 当前目标点的坐标
+	// PID的调用次数
+	let count = 0;
+    // 目标点的坐标位置
     let setpointX = x;
     let setpointY = y;
-    // Kd 坐标微分误差值 上一个时间节点的坐标值
+    // Kd 用以计算坐标微分误差值 上一个时间节点的坐标值
     let prevErrorX = 0;
     let prevErrorY = 0;
     // Ki 坐标积分误差值
@@ -37,27 +35,23 @@
     let integralY = 0;
 
     // 默认的Kp Ki Kd
-    let kp = 3.0;
-    let ki = 0.1;
-    // 设置最大的积分值 防止出现震荡过大的情况 不便于观察
-    let sat = 1000;
-    let kd = 80.0;
+    let kp = 0.9;
+    let ki = 0.8;
+    // 设置稳态误差 
+    let wind = 0.2;
+    let kd = 0.1;
 
-    let history = [];
-    let historyTick = 0;
-
-    // 坐标的变化量
+    // 坐标的误差值
     let deltaX = document.getElementById("deltaX");
     let deltaY = document.getElementById("deltaY");
+	let countInput = document.getElementById("count");
+	let xValue =  document.getElementById('x');
+	let yValue =  document.getElementById('y');
     
-    function saturate(x) {
-        // 当积分的绝对值大于最大积分值时 设置为最大积分值
-        return Math.min(Math.max(x,0-sat),sat)
-    }
-
     function pid() {
         // 当前X坐标的误差值
         let errorX = setpointX - x;
+		// 第一次调用PID的时候
         // Ki 误差积分(其实在离散的情况下就是做累加)
         integralX += errorX;
         // Kd 误差微分(当前时刻与上一时刻的误差的差值 肯定是负数)
@@ -65,7 +59,6 @@
         // 将当前的误差值赋值给上一时间，用作下一次计算的基础
         prevErrorX = errorX;
 
-        // 同理X坐标
         let errorY = setpointY - y;
         integralY += errorY;
         let derivativeY = errorY - prevErrorY;
@@ -73,56 +66,53 @@
 
         // 显示当前的误差值 保留三位小数
         // 这个加号是为了保证value为数字，document可能是字符串
-        deltaY.value = +errorY.toFixed(3);
-        deltaX.value = +errorX.toFixed(3);
-        // 对积分值进行判断
-        integralX=saturate(integralX);
-        integralY=saturate(integralY);
+        deltaY.value = +errorY.toFixed(5);
+        deltaX.value = +errorX.toFixed(5);
         // 返回PID控制值 缩小1000倍便于canvas上面观察
         let xCode = kp * errorX + ki * integralX + kd * derivativeX;
         let yCode = kp * errorY + ki * integralY + kd * derivativeY;
-        return [0.001 * xCode, 0.001 * yCode];
+		count++;
+		countInput.value = count;
+        return [xCode,yCode];
     }
-
+	//保留n位小数
+	function roundFun(value, n) {
+	  return Math.round(value*Math.pow(10,n))/Math.pow(10,n);
+	}
     // 更新canvas画面
     function update() {
         // 调用PID函数
-        let a = pid();
-        let ax = a[0];
-        let ay = a[1];
-        // 画布单次单向最大变化值，防止变化过快过大不利于观察
-        let maxA = 0.2;
-        // 取绝对值最大值
-        ax = Math.max(Math.min(ax, maxA), -maxA);
-        ay = Math.max(Math.min(ay, maxA), -maxA);
-        // 坐标的变化量
-        vx += ax;
-        vy += ay;
-        // 当前的坐标值
-        x += vx;
-        y += vy;
-
-        if (++historyTick == 5) {
-            historyTick = 0;
-
-            if (history.length >= 50) {
-                history.shift();
-            }
-            history.push([x, y])
-        }
+		let ax;
+		let ay;
+		if ( x !== setpointX && y !== setpointY){
+			// 设置误差为五位小数
+			if (Math.abs(x - setpointX) > 0.00001 && Math.abs(y - setpointY) > 0.00001){
+				let xCode = pid();
+				ax = xCode[0];
+				ay = xCode[1];
+				x = roundFun(x + ax - wind,5);
+				y = roundFun(y + ay - wind,5);
+				xValue.innerHTML += '<li>第'+ count + '次X为：' + x + '</li>';
+				yValue.innerHTML += '<li>第'+ count + '次Y为：' + y + '</li>';
+				setTimeout(update,200);
+			}
+			else{
+				errorY = 0;
+				deltaY.value = 0;
+				errorX = 0;
+				deltaX.value = 0;
+				console.log(count);
+				count = 0;
+			}
+		}
+		else{
+			console.log(count);
+			count = 0;
+		}
+		
         ctx.fillStyle = "#E7D492";
         ctx.fillRect(0, 0, c.width, c.height);
-
-        // 运动轨迹画圆
-        for (let i = 0; i < history.length; ++i) {
-            ctx.fillStyle = "rgba(96,185,154,"+(i/history.length)+")";
-            ctx.beginPath();
-            // 画圆
-            ctx.arc(history[i][0], history[i][1], 5, 0, 2 * Math.PI, true);
-            ctx.closePath();
-            ctx.fill();
-        }
-
+// 
         // 当前点与目标点之间的连线
         ctx.strokeStyle = "#60B99A";
         ctx.lineWidth = 1;
@@ -146,15 +136,12 @@
         ctx.lineCap = "round";
         ctx.beginPath()
         ctx.moveTo(x, y);
-        ctx.lineTo(x - ax * 300, y);
+        ctx.lineTo(x - ax * 0.3, y);
         ctx.stroke();
         ctx.beginPath()
         ctx.lineTo(x, y);
-        ctx.lineTo(x, y - ay * 300);
+        ctx.lineTo(x, y - ay * 0.3);
         ctx.stroke();
-
-        // 递归调用
-        setTimeout(update, 16);
     }
     update();
 
@@ -162,20 +149,25 @@
     function canvasClick(e) {
         setpointX = e.clientX;
         setpointY = e.clientY;
+		xValue.innerHTML = '';
+		yValue.innerHTML = '';
+		xValue.innerHTML += '<li>目标X坐标' + e.clientX + '</li>';
+		yValue.innerHTML += '<li>目标Y坐标' + e.clientY + '</li>';
+		update();
     }
     c.addEventListener("click", canvasClick);
 
     // 关键值
     let kpInput = document.getElementById("kp");
     let kiInput = document.getElementById("ki");
-    let satInput = document.getElementById("sat");
+    let windInput = document.getElementById("wind");
     let kdInput = document.getElementById("kd");
 
     // 每次改值的时候都更新系数
     function updateCoefficients() {
         kp = parseFloat(kpInput.value);
         ki = parseFloat(kiInput.value);
-        sat = parseFloat(satInput.value);
+        wind = parseFloat(windInput.value);
         kd = parseFloat(kdInput.value);
         integralX = 0;
         integralY = 0;
@@ -183,12 +175,13 @@
 
     kpInput.addEventListener("blur", updateCoefficients);
     kiInput.addEventListener("blur", updateCoefficients);
-    satInput.addEventListener("blur", updateCoefficients);
+    windInput.addEventListener("blur", updateCoefficients);
     kdInput.addEventListener("blur", updateCoefficients);
+	countInput.addEventListener("blur", updateCoefficients);
 
     kpInput.value = kp;
     kiInput.value = ki;
-    satInput.value = sat;
+    windInput.value = wind;
     kdInput.value = kd;
-
+	countInput.value = count;
 })();
